4411e9efaafe324d853ed007571a8e16f8c221b7
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2020-10-21 05:41:59 -0700

    Update success-datalayer.js

--- ecommerce/success-datalayer.js
+++ ecommerce/success-datalayer.js
@@ -1,7 +1,5 @@
-
+// Static Success Data Layer
 var window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
-'event': 'registrationComplete',
-'plan': 'Free'
-'storeId': '6'  
+'event': 'registrationComplete'
 });
