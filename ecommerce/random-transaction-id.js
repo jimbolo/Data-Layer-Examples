8c69e255ede41a32f77adb98a822f42f98b53ae7
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2020-10-21 02:02:02 -0700

    Update random-transaction-id.js

--- ecommerce/random-transaction-id.js
+++ ecommerce/random-transaction-id.js
@@ -3,22 +3,22 @@ function transaction_id_generator( len ) {
     var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
     var numeric = '0123456789';
     var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-    var password = "";
+    var transactionid = "";
     var character = "";
     var crunch = true;
-    while( password.length<length ) {
+    while( transactionid.length<length ) {
         entity1 = Math.ceil(string.length * Math.random()*Math.random());
         entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
         entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
         hold = string.charAt( entity1 );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+        hold = (transactionid.length%2==0)?(hold.toUpperCase()):(hold);
         character += hold;
         character += numeric.charAt( entity2 );
         character += punctuation.charAt( entity3 );
-        password = character;
+        transactionid = character;
     }
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return password.substr(0,len);
+    transactionid=transactionid.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return transactionid.substr(0,len);
 }
 
 console.log( transaction_id_generator() );
