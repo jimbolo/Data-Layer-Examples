46fcc213df85db601e7bd5c665c9b286e49a36bf
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2021-08-23 15:41:52 +0300

    Update transaction.js

--- ecommerce/transaction.js
+++ ecommerce/transaction.js
@@ -14,15 +14,19 @@ dataLayer.push({
       'products': [{
         'brand': 'PostScan Mail',   // Required.
         'name': 'Mailbox',          // Required. Use "Mailbox" as a name.
-        'category': 'Free',         // Required. Selected Plan Name. Insert one of the following: Free, Pay-As-You-Go, Basic, Starter, Standard, Premium
+        'category': 'Starter',      // Required. Selected Plan Name. Insert one of the following: Free, Pay-As-You-Go, Basic, Starter, Standard, Premium
         'id': '25',                 // Required. Selected Plan ID
-        'plan': 'Free',             // Required. dimmnesion1. Selected Plan Name. Insert one of the following: Free, Pay-As-You-Go, Basic, Starter, Standard, Premium
-        'price': '0.00',            // Required. Selected Plan Amount
+        'plan': 'Starter',          // Required. dimmnesion1. Selected Plan Name. Insert one of the following: Free, Pay-As-You-Go, Basic, Starter, Standard, Premium
+        'price': '15.00',           // Required. Selected Plan Amount
         'storeId': '6',             // Required. dimmnesion2. Selected Sub- Store ID
         'address': 'Anaheim CA',    // Required. dimmnesion3. Selected Store City and State
         'quantity': 1               // Required.
        }]
+      
+      
     }
   }
+  
+  
 });
 </script>
