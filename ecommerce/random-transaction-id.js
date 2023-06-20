91b0b577768dc84a4d75fe3cf3afd08236f66286
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2023-06-19 22:10:03 -0700

    transaction with dashes

--- ecommerce/random-transaction-id.js
+++ ecommerce/random-transaction-id.js
@@ -1,8 +1,8 @@
 function transaction_id_generator( len ) {
-    var length = (len)?(len):(10);
+    var length = (len)?(len):(38);
     var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
     var numeric = '0123456789';
-    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    var punctuation = '!@#$%';
     var transactionid = "";
     var character = "";
     var crunch = true;
@@ -18,7 +18,28 @@ function transaction_id_generator( len ) {
         transactionid = character;
     }
     transactionid=transactionid.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return transactionid.substr(0,len);
+    return transactionid.substring(0,len);
 }
 
 console.log( transaction_id_generator() );
+var count1 = transaction_id_generator().length;
+console.log( count1 );
+
+// now plit transaction_id_generator() into 4 parts
+// every 9 characters insert a dash
+// example: 1fdcbea7-6358-4865-a8c4
+ function regenerateWithDashes() {
+    var newString = transaction_id_generator();
+    var newStringWithDashes = '';
+    for (var i = 0; i < newString.length; i++) {
+        if (i % 9 == 0 && i != 0) {
+            newStringWithDashes += '-';
+        }
+        newStringWithDashes += newString[i];
+    }
+    return newStringWithDashes;
+}
+console.log( regenerateWithDashes() );
+// count the number of characters in the string of regenerateWithDashes()
+var count2 = regenerateWithDashes().length;
+console.log( count2 );
