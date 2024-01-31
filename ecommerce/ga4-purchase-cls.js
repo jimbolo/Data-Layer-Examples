2c9fb31a3a075424359df1d83ed9173eb4f0081c
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2024-01-31 12:51:20 +0300

    Update ga4-purchase-cls.js

--- ecommerce/ga4-purchase-cls.js
+++ ecommerce/ga4-purchase-cls.js
@@ -1,4 +1,4 @@
-// GA4 Purchase Reference:
+// GA4 Purchase Using Google Tag Manager:
 // https://developers.google.com/analytics/devguides/collection/ga4/ecommerce?client_type=gtm#purchase-gtm
 // https://developers.google.com/analytics/devguides/collection/ga4/reference/events?client_type=gtm#purchase
 
@@ -11,13 +11,13 @@ event: 'purchase',
                                 // It must not itself be PII (personally identifiable information). The value should never be persisted in Google Analytics cookies or other Analytics provided storage.
                                 // (Character Limit 256 and Type String)
         transaction_id: '1fdcbea7-6358-4865-a8c4-fab1eee51ed2', // Required. Unique ID for the transaction.
-        affiliation: '', // Name if it is available
         value: 40.00, // Required. Total of products prices below.
         tax: 0.00,  // Tax amount if it is available
         shipping: 0.00, // Shipping amount if it is available
         currency: 'USD', // Required. Currency of the transaction.
         coupon: '', // Coupon Name or ID if it is available
-            items: [{   item_id: 'inkcard_national', // Required. exp "FDLE Fingerprinting" or "FBI Fingerprinting"
+            items: [{
+            item_id: 'inkcard_national', // Required. exp "FDLE Fingerprinting" or "FBI Fingerprinting"
             item_brand: 'UPS Store 125', // Required.
             item_name: 'Certifix Booking', // Required.
             item_category: 'Sterling', // Required.
@@ -26,7 +26,7 @@ event: 'purchase',
             item_variant: '',
             item_list_id: '', //related_products
             item_list_name: '', //Related Products
-            affiliation: '',
+            affiliation: '',  // Name if it is available
             coupon: '',
             discount: 0.00,
             price: 40.00, // Required.
