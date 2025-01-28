776b9471d741f2ac18747b56560def7c32a6f74b
Jimbolo <38487442+jimbolo@users.noreply.github.com>
2025-01-28 19:41:01 +0300

    Update login_dataLayer.js

--- ecommerce/login_dataLayer.js
+++ ecommerce/login_dataLayer.js
@@ -13,14 +13,14 @@ Email (preferred)
 A phone number can also be provided as a standalone match key but is recommended to be sent along with an email.
 Note that if your site doesn't collect one of those fields, remove the field entirely rather than leaving it blank. 
 
- If you decide to normalize and hash the data, follow the below instructions.
+If you decide to normalize and hash the data, follow the below instructions.
 For normalization: Remove leading or trailing whitespaces. Convert the text to lowercase.
 
 The phone number must be in E.164 format,
 which means it must be 11 to 15 digits including a plus sign (+) prefix
 and country code with no dashes, brackets or spaces. exp: +14155552671
 
-For hash: Use hex SHA256. https://support.google.com/google-ads/answer/9004655
+For hash: Use hex SHA256
 
 Validate your implementation using Chrome Developer Tools
 https://support.google.com/google-ads/answer/13262500?#zippy=%2Cenable-enhanced-conversions-in-google-tag-manager-and-create-custom-javascript-variable%2Cidentify-and-define-your-enhanced-conversions-variables%2Cvalidate-your-implementation-using-chrome-developer-tools
@@ -36,8 +36,6 @@ dataLayer.push({
   'sha256_email_address': 'yourNormalizedandHashedEmailVariable',
   'sha256_phone_number': 'yourNormalizedandHashedPhoneVariable'
 });
-
-
 
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
